4e81ecf98331ba6c1c84c01fce4b3e18d8523d18
susantodwi2 <61494555+susantodwi2@users.noreply.github.com>
2020-06-07 04:02:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
-
-$channelSecret = 'CHANNEL SECRET';
+$channelAccessToken = 'ajqgeFKGSmbCd8P2MbB9XtrUcUbvruwNLlVip5eu4jHMlwQPLmtnjCyBJsZxWAwIrJcVK3teAhfuiwCdTqKggKQ8RlfM+UTlJqe0Ai/ybUgdoOC5nx6tTBO7JY0r2E1X90IxdEc/7CpddiK8lz2x3gdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '7a7e5e56bae79c8520441571720a6c7a';
